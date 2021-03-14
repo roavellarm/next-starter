4eb7cbd8f059e9973b62d33c545fc46be7254eac
Rodrigo Avellar <roavellarm@gmail.com>
2021-03-14 12:19:09 -0300

    Extract Global styles and theme to styles

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,12 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-import 'styled-components'
-
-declare module 'styled-components' {
-  export interface DefaultTheme {
-    colors: {
-      primary: string
-    }
-  }
-}
---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body {
+    margin: 0 auto;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+`
+
+export default GlobalStyle
