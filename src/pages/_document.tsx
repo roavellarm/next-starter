8d84eb45312b167f39bb375423e7788696acd76e
Rodrigo Avellar <roavellarm@gmail.com>
2021-03-14 19:07:47 -0300

    Add lint-staged and husky

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
-    try {
+    try {                   
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
