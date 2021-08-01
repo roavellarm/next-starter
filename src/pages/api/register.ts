4c293dd428bbf7f894e8af6db0fc6683e7dc55c0
Rodrigo Avellar <roavellarm@gmail.com>
2021-08-01 14:22:26 -0300

    Add salt key to md5

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -7,7 +7,7 @@ const register = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const user = await User.create({
       email: req.body.email,
-      password: md5(req.body.password),
+      password: md5(req.body.password + process.env.SALT_KEY),
     })
 
     return res.status(200).send({ user })
